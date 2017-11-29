ddb86091594de82c0537e988b1b0ac3b9ad52e72
ipunganwar <ipung.anwar1@gmail.com>
2017-11-29 20:29:38 +0700

    tambah png

--- client/src/components/coba.vue
+++ client/src/components/coba.vue
@@ -7,19 +7,28 @@ import axios from 'axios'
 export default {
   data () {
     return {
-      random: ['google.com','djarum.com','unilever.com','bumn.go.id' ],
+      random: [ 'google.com', 'djarum.com', 'unilever.com', 'bumn.go.id', 'bni.co.id' ],
       list: {
         google: 'google.com',
         djarum: 'djarum.com',
         unilever: 'unilever.com',
         bumn: 'bumn.go.id'
-      } 
+      }
     }
   },
-created () {
-  axios.get('https://autocomplete.clearbit.com/v1/companies/suggest?query=google')
-    .then(response => {
-      console.log(response)
+  methods: {
+    RandomGenerator () {
+      let hasil = Math.floor(Math.random() * this.random.length)
+      return hasil
+    }
+  },
+  created () {
+    console.log()
+    axios.get('https://autocomplete.clearbit.com/v1/companies/suggest?query=' 
+    + this.random[this.RandomGenerator()] + '&size=200')
+    // axios.get('https://logo.clearbit.com/google.com')
+    .then(({data}) => {
+      console.log(data)
     })
     .catch(err => {
       console.log(err)
