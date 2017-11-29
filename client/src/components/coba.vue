b4794b4c8a71ed2f93eded07ac7771c7c7f872c5
azhariemuhammad <azharie_muhammad@live.com>
2017-11-29 20:28:52 +0700

    minor change

--- client/src/components/coba.vue
+++ client/src/components/coba.vue
@@ -3,6 +3,7 @@
     <textarea name="name" v-model="domain" rows="8" cols="80">Masukan Domain</textarea>
     <button type="button" value="submit" @click="getDomain" name="button">Submit</button>
   </div>
+  <img :src="clearbit" alt="foo">
 
 </template>
 
@@ -21,6 +22,7 @@
         axios.get(`https://logo.clearbit.com/${this.domain}`)
         .then(response => {
           console.log(response.data)
+          this.clearbit = response.data
         })
         .catch(err => {
           console.error(err)
