92f2cc4b8ed9d7f85ebf4a2f829fab6c39beb96a
ipunganwar <ipung.anwar1@gmail.com>
2017-11-29 22:22:24 +0700

    erge

--- client/src/components/Play.vue
+++ client/src/components/Play.vue
@@ -3,8 +3,8 @@
     <div class="row col-md-4 col-md-offset-4">
     <div id="">
       <div class="row">
-        <div style="text-align: center; margin-left: 22%;" class="form-group logologo">
-          <p style="padding:30%;">logo disini</p>
+        <div style="text-align: center; margin-left: 22%;" class="form-group logologo" >
+          <img style="padding:30%;" v-bind:src="getFirebase"></img>
         </div>
       </div>
       <form style="padding-top: 20%;" action="">
@@ -12,16 +12,77 @@
         <div style="text-align: center; margin-left: 20%;" class="form-group">
           <input type="text" class="form-control" id="exampleInputEmail1" placeholder="tebak nama logo">
         </div>
-      <button type="submit" class="btn btn-primary">TEBAK</button>
+      <button type="submit" class="btn btn-primary">{{ getFirebase }}</button>
       </form>
     </div>
   </div>
+
+  <p>{{ getFirebase }}</p>
   </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios'
+import db from '../firebase'
 
+export default {
+  data () {
+    return {
+      status: '',
+      tebakan: '',
+      jawaban: [],
+      random: [ 'google.com', 'djarum.com', 'unilever.com', 'bumn.go.id', 'bni.co.id' ],
+      list: [{
+          google: 'google.com',
+          djarum: 'djarum.com',
+          unilever: 'unilever.com',
+          bumn: 'bumn.go.id'
+      }],
+      tes: ''
+    }
+  },
+  methods: {
+    GetLogo () {
+      let id = this.random[this.RandomGenerator()]
+      axios.get('https://autocomplete.clearbit.com/v1/companies/suggest?query=' 
+      + id )
+      // axios.get('https://logo.clearbit.com/google.com')
+      .then(({data}) => {
+        console.log(data[0])
+        this.setFirebase (data[0].logo, data[0].name)
+        this.jawaban = data
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    },
+    RandomGenerator () {
+      return Math.floor(Math.random() * this.random.length)
+    },
+    Tebakan () {
+      console.log(this.jawaban[0].name)
+      if(this.tebakan == this.jawaban[0].name){
+        this.status = 'Benar' 
+        this.GetLogo()
+      }
+      else{
+        this.status = 'Salah'
+      }
+    },
+    setFirebase (imgUrl, name) {
+      db.ref('gamequiz').set({
+        imgUrl: imgUrl,
+        name: name
+      })
+    },
+  },
+  computed: {
+    getFirebase () {
+      db.ref('gamequiz').on('value', function(response) {
+        return response.val().imgUrl
+      })
+    }
+  }
 }
 </script>
 
