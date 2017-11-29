0859c6455fd1abea5e1fb22edfb8744f6c08a9a5
ipunganwar <ipung.anwar1@gmail.com>
2017-11-29 20:03:11 +0700

    commitan pertama

---
+++ client/src/components/coba.vue
@@ -0,0 +1,33 @@
+<template>
+  
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      random: ['google.com','djarum.com','unilever.com','bumn.go.id' ],
+      list: {
+        google: 'google.com',
+        djarum: 'djarum.com',
+        unilever: 'unilever.com',
+        bumn: 'bumn.go.id'
+      } 
+    }
+  },
+created () {
+  axios.get('https://autocomplete.clearbit.com/v1/companies/suggest?query=google')
+    .then(response => {
+      console.log(response)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import coba from '@/components/coba'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'coba',
+      component: coba
     }
   ]
 })
