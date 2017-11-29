89b1eb05d67ee8c145f4bce0ed5ec9a56f4ef361
azhariemuhammad <azharie_muhammad@live.com>
2017-11-29 20:10:06 +0700

    clearbit api

---
+++ client/src/components/logo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="form">
+    <textarea name="name" v-model="domain" rows="8" cols="80">Masukan Domain</textarea>
+    <button type="button" value="submit" @click="getDomain" name="button">Submit</button>
+  </div>
+
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    name: 'Clearbit',
+    data () {
+      return {
+        clearbit: '',
+        domain: ''
+      }
+    },
+    methods: {
+      getDomain: function () {
+        axios.get(`https://logo.clearbit.com/${this.domain}`)
+        .then(response => {
+          console.log(response.data)
+        })
+        .catch(err => {
+          console.error(err)
+        })
+      }
+    }
+  }
+</script>
