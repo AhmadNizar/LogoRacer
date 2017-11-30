d2c7c91a506291398bea8b2a11265eb5806777ca
aditReza <lete.note3@gmail.com>
2017-11-30 13:36:30 +0700

    game over

--- client/src/components/Play.vue
+++ client/src/components/Play.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <Modal></Modal>
     <div class="row">
       <div class="col-md-6 offset-md-3">
         <img class="" style="padding:10%;" :src="imageurl"></img>
@@ -24,13 +25,13 @@
         <p style="font-size:13px;" class="mb-0">Whenever you need</p>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 import db from '../firebase'
+import Modal from './Modal'
 
 export default {
   data () {
@@ -52,6 +53,9 @@ export default {
       nyawa: 3,
       gameOver: false
     }
+  },
+  components: {
+    Modal
   },
   methods: {
     GetLogo () {
@@ -83,6 +87,10 @@ export default {
           console.log('game over')
           this.gameOver = true
         }
+      }
+      if (this.gameOver === true) {
+        console.log('looooooooseeee')
+        $('#myModal').modal('show')
       }
     },
     setFirebase (imgUrl, name) {
